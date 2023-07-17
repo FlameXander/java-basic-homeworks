b50e016612f7df06eb85d9826a47c0e1b3481345
Alexander Fisunov <fisunov.a.v@gmail.com>
2023-07-17 22:16:40 +0300

    homeworkN fixed

--- src/main/java/ru/otus/java/basic/homeworks/lessonN/MainApp.java
+++ src/main/java/ru/otus/java/basic/homeworks/lessonN/MainApp.java
@@ -3,5 +3,6 @@ package ru.otus.java.basic.homeworks.lessonN;
 public class MainApp {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("123");
     }
 }
